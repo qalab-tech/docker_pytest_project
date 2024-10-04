952b4ba3f5ae67f96aefd8c2b18b0f0edcbd1c48
Dmitrii Gornostaev <dmitry.gornostaev@gmail.com>
2024-10-04 14:22:30 +0300

    New assertion method for OPTIONS HTTP Method Check

--- tests/head_and_option_test.py
+++ tests/head_and_option_test.py
@@ -10,12 +10,22 @@ with open("tests/pytests_config.yaml", 'r') as file:
 # Flask microservice Base URL
 BASE_URL = config["Base Application URL"]
 
+#
+# def test_options():
+#     # Test Options HTTP Method
+#     response = requests.options(BASE_URL)
+#     assert response.status_code == 200
+#     assert response.headers['Allow'] == "OPTIONS, GET, POST, HEAD"
 
 def test_options():
-    # Test Options HTTP Method
+    # Пример ожидаемого ответа
+    expected_methods = 'OPTIONS, GET, POST, HEAD'
     response = requests.options(BASE_URL)
-    assert response.status_code == 200
-    assert response.headers['Allow'] == "OPTIONS, GET, POST, HEAD"
+    actual_methods = response.headers['Allow']
+
+    # Convert both strings to sets and compare
+    assert set(actual_methods.split(', ')) == set(expected_methods.split(', ')), f"Expected: {expected_methods}, but got: {actual_methods}"
+
 
 def test_head():
     # Test HEAD HTTP Method
